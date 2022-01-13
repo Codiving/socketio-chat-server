4372f299f5a25301f61eefee64146003ebe7f2d3
Codiving <codivingmoon@gmail.com>
2022-01-13 19:18:34 +0900

    1. 초기 프로젝트 정리하기

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import cors from "cors";
+import * as dotenv from "dotenv";
+import express from "express";
+import http from "http";
+import SocketIO from "socket.io";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded());
+app.use(express.json());
+
+const serverListening = () => {
+  console.log(`# server is running : http://localhost:${PORT}`);
+};
+
+const httpServer = http.createServer(app);
+const io = SocketIO(httpServer);
+httpServer.listen(PORT, serverListening);
+
+io.on("connection", socket => {});
