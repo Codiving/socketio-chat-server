baaf9cd90eff92d0a1e54060be6aa65964adae50
Codiving <codivingmoon@gmail.com>
2022-01-15 14:45:16 +0900

    3. 채팅방 생성 / 동기화

--- src/index.js
+++ src/index.js
@@ -21,7 +21,50 @@ const httpServer = http.createServer(app);
 const io = SocketIO(httpServer);
 httpServer.listen(PORT, serverListening);
 
+const getSids = () => {
+  const ids = [];
+  const { sids, rooms } = io.of("/").adapter;
+
+  rooms.forEach((_, key) => {
+    if (sids.get(key)) {
+      ids.push(key);
+    }
+  });
+
+  return ids;
+};
+
+const getUserRooms = () => {
+  const userRooms = [];
+  const { sids, rooms } = io.of("/").adapter;
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      userRooms.push(key);
+    }
+  });
+
+  return userRooms;
+};
+
+const updateRoomList = () => {
+  const ids = getSids();
+  const userRooms = getUserRooms();
+
+  ids.forEach(id => io.to(id).emit("updateRooms", userRooms));
+};
+
 io.on("connection", socket => {
+  socket.on("login", () => {
+    io.to(socket.id).emit("updateRooms", getUserRooms());
+  });
+
+  socket.on("create-room", (roomName, done) => {
+    console.log("create-room", roomName);
+    socket.join(roomName);
+    done();
+    updateRoomList();
+  });
+
   socket.on("nickname", (nickname, done) => {
     socket["nickname"] = nickname;
     done();
